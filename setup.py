cd80f156829b6e352fa8c3ae7ed20269f32fa22b
Kade <kade-robertson@users.noreply.github.com>
2019-03-21 08:58:45 -0400

    Update setup to reflect new version compatibility (#5)
    Fixes #4

--- setup.py
+++ setup.py
@@ -13,9 +13,6 @@ setup(
     long_description_content_type='text/markdown',
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -30,4 +27,4 @@ setup(
     packages=find_packages(),
     install_requires=[],
     test_suite='tests',
-    python_requires='>=2.7.3, !=3.0.*, !=3.1.*, !=3.2.*')
+    python_requires='>=3.5.*')
