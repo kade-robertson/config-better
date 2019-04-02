3285ea47629cdec250b69daec132e10058909e52
Kade <kade-robertson@users.noreply.github.com>
2019-04-02 11:22:22 -0400

    Make testing more consistent (#10)

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,121 +1,148 @@
 import os
+import shutil
 import sys
+import tempfile
 import unittest
 
 import configbetter
 
-        
+
 class TestMakedirs(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         configbetter.sys.platform = sys.platform
+        cls.tempdir = tempfile.mkdtemp()
         if sys.platform in ['linux', 'darwin']:
-            configbetter.os.environ['HOME'] = os.path.expanduser('~')
+            configbetter.os.environ['HOME'] = cls.tempdir
         elif sys.platform == 'win32':
-            configbetter.os.environ['APPDATA'] = os.environ['APPDATA']
+            configbetter.os.environ['APPDATA'] = cls.tempdir
         if 'XDG_DATA_HOME' in configbetter.os.environ:
             del configbetter.os.environ['XDG_DATA_HOME']
         if 'XDG_CONFIG_HOME' in configbetter.os.environ:
             del configbetter.os.environ['XDG_CONFIG_HOME']
         if 'XDG_CACHE_HOME' in configbetter.os.environ:
             del configbetter.os.environ['XDG_CACHE_HOME']
-        print(configbetter.os.environ)
-        self.conf = configbetter.Config('notarealapp')
-        self.conf.makedirs()
-   
-    def tearDown(self):
-        if os.path.exists(self.conf.cache):
-            os.rmdir(self.conf.cache)
-        if os.path.exists(self.conf.config):
-            os.rmdir(self.conf.config)
-        if os.path.exists(self.conf.data):
-            os.rmdir(self.conf.data)
-    
-    def test_data_dir(self):
-        self.assertTrue(os.path.exists(self.conf.data))
-        
-    def test_config_dir(self):
-        self.assertTrue(os.path.exists(self.conf.config))
-        
-    def test_cache_dir(self):
-        self.assertTrue(os.path.exists(self.conf.cache))
+        cls.conf = configbetter.Config('notarealapp')
+        cls.conf.makedirs()
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.tempdir)
+
+    def test_data_dir(cls):
+        cls.assertTrue(os.path.exists(cls.conf.data))
+
+    def test_config_dir(cls):
+        cls.assertTrue(os.path.exists(cls.conf.config))
+
+    def test_cache_dir(cls):
+        cls.assertTrue(os.path.exists(cls.conf.cache))
 
 
 class TestWindowsNoXDG(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         configbetter.sys.platform = 'win32'
-        configbetter.os.environ['APPDATA'] = 'C:\\fakedir'
-        self.conf = configbetter.Config('fakeapp')
+        cls.tempdir = tempfile.mkdtemp()
+        configbetter.os.environ['APPDATA'] = cls.tempdir
+        cls.conf = configbetter.Config('fakeapp')
 
-    def test_noxdg_data(self):
-        checkdata = os.path.join('C:\\fakedir', 'fakeapp', 'Data')
-        self.assertEqual(self.conf.data, checkdata)
+    @classmethod
+    def tearDownClass(cls):
+        os.rmdir(cls.tempdir)
 
-    def test_noxdg_config(self):
-        checkconfig = os.path.join('C:\\fakedir', 'fakeapp', 'Config')
-        self.assertEqual(self.conf.config, checkconfig)
+    def test_noxdg_data(cls):
+        checkdata = os.path.join(cls.tempdir, 'fakeapp', 'Data')
+        cls.assertEqual(cls.conf.data, checkdata)
 
-    def test_noxdg_cache(self):
-        checkcache = os.path.join('C:\\fakedir', 'fakeapp', 'Cache')
-        self.assertEqual(self.conf.cache, checkcache)
+    def test_noxdg_config(cls):
+        checkconfig = os.path.join(cls.tempdir, 'fakeapp', 'Config')
+        cls.assertEqual(cls.conf.config, checkconfig)
+
+    def test_noxdg_cache(cls):
+        checkcache = os.path.join(cls.tempdir, 'fakeapp', 'Cache')
+        cls.assertEqual(cls.conf.cache, checkcache)
 
 
 class TestLinuxNoXDG(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         configbetter.sys.platform = 'linux'
-        configbetter.os.environ['HOME'] = '/fakedir'
-        self.conf = configbetter.Config('fakeapp')
+        cls.tempdir = tempfile.mkdtemp()
+        configbetter.os.environ['HOME'] = cls.tempdir
+        cls.conf = configbetter.Config('fakeapp')
 
-    def test_noxdg_data(self):
-        checkdata = os.path.join('/fakedir', '.local', 'share', 'fakeapp')
-        self.assertEqual(self.conf.data, checkdata)
+    @classmethod
+    def tearDownClass(cls):
+        os.rmdir(cls.tempdir)
 
-    def test_noxdg_config(self):
-        checkconfig = os.path.join('/fakedir', '.config', 'fakeapp')
-        self.assertEqual(self.conf.config, checkconfig)
+    def test_noxdg_data(cls):
+        checkdata = os.path.join(cls.tempdir, '.local', 'share', 'fakeapp')
+        cls.assertEqual(cls.conf.data, checkdata)
 
-    def test_noxdg_cache(self):
-        checkcache = os.path.join('/fakedir', '.cache', 'fakeapp')
-        self.assertEqual(self.conf.cache, checkcache)
+    def test_noxdg_config(cls):
+        checkconfig = os.path.join(cls.tempdir, '.config', 'fakeapp')
+        cls.assertEqual(cls.conf.config, checkconfig)
+
+    def test_noxdg_cache(cls):
+        checkcache = os.path.join(cls.tempdir, '.cache', 'fakeapp')
+        cls.assertEqual(cls.conf.cache, checkcache)
 
 
 class TestMacNoXDG(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         configbetter.sys.platform = 'darwin'
-        configbetter.os.environ['HOME'] = '/fakedir'
-        self.conf = configbetter.Config('fakeapp')
+        cls.tempdir = tempfile.mkdtemp()
+        configbetter.os.environ['HOME'] = cls.tempdir
+        cls.conf = configbetter.Config('fakeapp')
 
-    def test_noxdg_data(self):
-        checkdata = os.path.join('/fakedir', 'Library', 'fakeapp')
-        self.assertEqual(self.conf.data, checkdata)
+    @classmethod
+    def tearDownClass(cls):
+        os.rmdir(cls.tempdir)
 
-    def test_noxdg_config(self):
-        checkconfig = os.path.join('/fakedir', 'Library', 'Preferences', 'fakeapp')
-        self.assertEqual(self.conf.config, checkconfig)
+    def test_noxdg_data(cls):
+        checkdata = os.path.join(cls.tempdir, 'Library', 'fakeapp')
+        cls.assertEqual(cls.conf.data, checkdata)
 
-    def test_noxdg_cache(self):
-        checkcache = os.path.join('/fakedir', 'Library', 'Caches', 'fakeapp')
-        self.assertEqual(self.conf.cache, checkcache)
+    def test_noxdg_config(cls):
+        checkconfig = os.path.join(cls.tempdir, 'Library', 'Preferences', 'fakeapp')
+        cls.assertEqual(cls.conf.config, checkconfig)
+
+    def test_noxdg_cache(cls):
+        checkcache = os.path.join(cls.tempdir, 'Library', 'Caches', 'fakeapp')
+        cls.assertEqual(cls.conf.cache, checkcache)
 
 
 class TestXDG(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         configbetter.sys.platform = 'win32'
-        configbetter.os.environ['XDG_DATA_HOME'] = '/xdgdatadir'
-        configbetter.os.environ['XDG_CONFIG_HOME'] = '/xdgconfigdir'
-        configbetter.os.environ['XDG_CACHE_HOME'] = '/xdgcachedir'
-        self.conf = configbetter.Config('fakeapp')
+        cls.datatempdir = tempfile.mkdtemp()
+        cls.configtempdir = tempfile.mkdtemp()
+        cls.cachetempdir = tempfile.mkdtemp()
+        configbetter.os.environ['XDG_DATA_HOME'] = cls.datatempdir
+        configbetter.os.environ['XDG_CONFIG_HOME'] = cls.configtempdir
+        configbetter.os.environ['XDG_CACHE_HOME'] = cls.cachetempdir
+        cls.conf = configbetter.Config('fakeapp')
 
-    def test_xdg_data(self):
-        checkdata = os.path.join('/xdgdatadir', 'fakeapp')
-        self.assertEqual(self.conf.data, checkdata)
+    @classmethod
+    def tearDownClass(cls):
+        os.rmdir(cls.datatempdir)
+        os.rmdir(cls.configtempdir)
+        os.rmdir(cls.cachetempdir)
 
-    def test_xdg_config(self):
-        checkconfig = os.path.join('/xdgconfigdir', 'fakeapp')
-        self.assertEqual(self.conf.config, checkconfig)
+    def test_xdg_data(cls):
+        checkdata = os.path.join(cls.datatempdir, 'fakeapp')
+        cls.assertEqual(cls.conf.data, checkdata)
 
-    def test_xdg_cache(self):
-        checkcache = os.path.join('/xdgcachedir', 'fakeapp')
-        self.assertEqual(self.conf.cache, checkcache)
+    def test_xdg_config(cls):
+        checkconfig = os.path.join(cls.configtempdir, 'fakeapp')
+        cls.assertEqual(cls.conf.config, checkconfig)
+
+    def test_xdg_cache(cls):
+        checkcache = os.path.join(cls.cachetempdir, 'fakeapp')
+        cls.assertEqual(cls.conf.cache, checkcache)
 
 
 if __name__ == '__main__':
